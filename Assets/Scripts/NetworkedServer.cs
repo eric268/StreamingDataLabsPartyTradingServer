5b515bb48ad6ae9eb894a1648779c9bba34c3d5d
Eric Galway <ericwilliamgalway@gmail.com>
2021-11-16 18:44:16 -0500

    Added creation or searching for sharing room

--- Assets/Scripts/NetworkedServer.cs
+++ Assets/Scripts/NetworkedServer.cs
@@ -13,6 +13,8 @@ public class NetworkedServer : MonoBehaviour
     int unreliableChannelID;
     int hostID;
     int socketPort = 5491;
+    List<SharingRoom> sharingRooms;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +25,7 @@ public class NetworkedServer : MonoBehaviour
         unreliableChannelID = config.AddChannel(QosType.Unreliable);
         HostTopology topology = new HostTopology(config, maxConnections);
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
+        sharingRooms = new List<SharingRoom>();
 
     }
 
@@ -68,6 +71,54 @@ public class NetworkedServer : MonoBehaviour
     private void ProcessRecievedMsg(string msg, int id)
     {
         Debug.Log("msg recieved = " + msg + ".  connection id = " + id);
+
+        string[] csv = msg.Split(',');
+        int signifier =int.Parse(csv[0]);
+
+        if (signifier == ClientToServerSignifiers.JoinSharingRoom)
+        {
+            string roomName = csv[1];
+            bool hasBeenFound = false;
+            for (int i = 0; i < sharingRooms.Count;i++)
+            {
+                if (sharingRooms[i].name == roomName)
+                {
+                    hasBeenFound = true;
+                    sharingRooms[i].connectionID.Add(id);
+                    Debug.Log("Room Joined");
+                    break;
+                }
+            }
+            if (!hasBeenFound)
+            {
+                Debug.Log("Room Created");
+                sharingRooms.Add(new SharingRoom(roomName, id));
+            }
+        }
     }
+
+}
+
+public class SharingRoom
+{
+    public string name;
+    public List<int> connectionID;
+
+    public SharingRoom(string n, int creatorID)
+    {
+        name = n;
+        connectionID = new List<int>();
+        connectionID.Add(creatorID);
+    }
+
+}
+
+static public class ClientToServerSignifiers
+{
+    public const int JoinSharingRoom = 1;
+}
+
+static public class ServerToClientSignifiers
+{
 
 }
